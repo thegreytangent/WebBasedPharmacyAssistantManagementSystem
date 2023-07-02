82790ca944f3b92740768c9acf7595000f3a0597
thegreytangent <thegreytangent@gmail.com>
2023-07-02 15:11:25 +0800

    Feat: Working on design sidebar and dashboard

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -3,8 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\View\View;
 
 class DashboardController extends Controller
 {
+
+    public function index() : View {
+        return view('dashboard.index');
+    }
 
 }
---
+++ resources/views/template/sidebar.blade.php
@@ -0,0 +1,49 @@
+<div class="sidebar-wrapper" data-simplebar="true">
+    <div class="sidebar-header">
+        <div>
+            <img src="assets/images/logo-icon.png" class="logo-icon" alt="logo icon">
+        </div>
+        <div>
+            <h4 class="logo-text">Rocker</h4>
+        </div>
+        <div class="toggle-icon ms-auto"><i class='bx bx-arrow-back'></i>
+        </div>
+    </div>
+    <!--navigation-->
+    <ul class="metismenu" id="menu">
+        <li>
+            <a href="https://codervent.com/rocker/documentation/index.html" target="_blank">
+                <div class="parent-icon"><i class='bx bx-home-alt'></i>
+                </div>
+                <div class="menu-title">Dashboard</div>
+            </a>
+        </li>
+
+
+
+        <li>
+            <a href="https://codervent.com/rocker/documentation/index.html" target="_blank">
+                <div class="parent-icon"><i class="bx bx-folder"></i>
+                </div>
+                <div class="menu-title">Documentation</div>
+            </a>
+        </li>
+        <li>
+            <a href="javascript:;">
+                <div class="parent-icon"><i class="bx bx-home-alt"></i>
+                </div>
+                <div class="menu-title">Reports</div>
+            </a>
+            <ul class="mm-collapse mm-show" style="">
+                <li> <a href="index.html"><i class="bx bx-radio-circle"></i>Default</a>
+                </li>
+                <li> <a href="index2.html"><i class="bx bx-radio-circle"></i>Alternate</a>
+                </li>
+                <li> <a href="index3.html"><i class="bx bx-radio-circle"></i>Graphical</a>
+                </li>
+            </ul>
+        </li>
+
+    </ul>
+    <!--end navigation-->
+</div>
