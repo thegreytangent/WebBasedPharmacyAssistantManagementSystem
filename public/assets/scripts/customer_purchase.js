300dc620ebbaa9843f2ccfcc87da34e0efd5666b
thegreytangent <thegreytangent@gmail.com>
2023-12-17 18:26:20 +0800

    Feat: Back reload in customer purchase

--- public/assets/scripts/customer_purchase.js
+++ public/assets/scripts/customer_purchase.js
@@ -54,7 +54,7 @@ $(document).ready(function () {
                 dataType: 'JSON',
                 data:  JSON.stringify(purchases),
                 success: function(data) {
-                    // location.reload();
+                    location.reload();
                 }
             });
         }
