e5ba94845af925053d0ee21b78e2a496235d744a
thegreytangent <thegreytangent@gmail.com>
2024-01-15 21:32:46 +0800

    Fix:sidebar url

--- resources/views/template/sidebar_customer.blade.php
+++ resources/views/template/sidebar_customer.blade.php
@@ -1,6 +1,6 @@
 <li class="menu-label">Manage Records</li>
 <li>
-    <a href="customer/purchase">
+    <a href="{{config('app.url')}}/customer/purchase">
         <div class="parent-icon"><i class='bx bx-home-alt'></i>
         </div>
         <div class="menu-title">View Purchases</div>
