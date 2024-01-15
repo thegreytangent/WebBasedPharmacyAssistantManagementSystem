1aa1e2b4415abc797715b0573c670a5ff4792eab
thegreytangent <thegreytangent@gmail.com>
2024-01-15 22:01:48 +0800

    Redundant changes in code

--- resources_backup/views/template/sidebar_customer.blade.php
+++
@@ -1,8 +0,0 @@
-<li class="menu-label">Manage Records</li>
-<li>
-    <a href="customer/purchase">
-        <div class="parent-icon"><i class='bx bx-home-alt'></i>
-        </div>
-        <div class="menu-title">View Purchases</div>
-    </a>
-</li>
